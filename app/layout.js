aa50dff2ee39201029b3906bd82b2bae4ca9d2c6
m06197889-cyber <m06197889@gmail.com>
2026-08-29 00:10:13 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+import './globals.css';
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl">
       <body>{children}</body>
     </html>
   )
